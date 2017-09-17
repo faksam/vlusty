77c5b696e004025bab33484b5b5979053e65eb04
faksam <fakunlesamuel@gmail.com>
2017-09-17 03:05:27 +0100

    Add new public files and update index.html

---
+++ public/javascripts/Script.js
@@ -0,0 +1,13 @@
+﻿(function ($) {
+    $(function () {
+        $('.carousel.carousel-slider').carousel({ fullWidth: true });
+        $('.carousel.carousel-slider').carousel({ fullWidth: true, padding: 200 }, setTimeout(autoplay, 5500));
+        function autoplay() {
+            $('.carousel').carousel('next');
+            setTimeout(autoplay, 5500);
+        };
+        $('.carousel').carousel({ duration: 35 });
+        $('.button-collapse').sideNav();
+    });
+
+})(jQuery);
